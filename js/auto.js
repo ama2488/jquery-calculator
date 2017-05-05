9dfd6e04cae78eedee1f6490cf89318e522407af
Amanda Allen <amanda.allen2488@gmail.com>
2017-05-05 08:39:47 -0500

    fixed divide by zero error

--- js/auto.js
+++ js/auto.js
@@ -6,58 +6,42 @@ $(() => {
   let operator = '+';
   let screenValue = '';
 
-  $screen.focus();
-
-  // Add input validation to only allow numbers and operators
-  $screen.keydown((e) => {
-    // Allow: backspace, delete, escape and enter
-    if ($.inArray(e.keyCode, [8, 9, 27, 13, 110]) !== -1 ||
-    // Allow .
-    (!e.shiftKey && e.keyCode === 190) ||
-    // Allow: Ctrl+A, Command+A
-    (e.keyCode === 65 && (e.ctrlKey === true || e.metaKey === true)) ||
-    // Allow: home, end, left, right, down, up
-    (e.keyCode >= 35 && e.keyCode <= 40)) {
-      return;
-    }
-    if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
-      e.preventDefault();
-    }
-  });
-
-
   const calculator = { '+': function add(b) {
     calc += b;
   },
     '-': function subtract(b) {
       calc -= b;
-      calc = calc.toFixed(2);
+      calc = parseFloat(calc.toFixed(2));
     },
     x: function multiply(b) {
       if (calc === 0) {
         calc = 1;
       }
       calc *= b;
+      calc = parseFloat(calc.toFixed(2));
     },
     '÷': function divide(b) {
       if (b === 0) {
         calc = 'error';
+      } else {
+        calc /= b;
+        calc = parseFloat(calc.toFixed(2));
       }
-      calc /= b;
-      calc = parseFloat(calc.toFixed(2));
     },
     '/': function divide(b) {
       if (b === 0) {
         calc = 'error';
+      } else {
+        calc /= b;
+        calc = parseFloat(calc.toFixed(2));
       }
-      calc /= b;
-      calc = parseFloat(calc.toFixed(2));
     },
     '*': function multiply(b) {
       if (calc === 0) {
         calc = 1;
       }
       calc *= b;
+      calc = parseFloat(calc.toFixed(2));
     },
     '': function restart(b) {
       calc = b;
@@ -65,18 +49,21 @@ $(() => {
   };
 
   $screen.keyup(function calculateString(e) {
+    // press esc
     if (e.which === 27) {
       calc = 0;
       $screen.val('');
       operator = '+';
       $screen.focus();
       screenValue = '';
+      // press enter
     } else if (e.which === 13) {
       $(this).submit();
       screenValue = parseFloat($(this)[0].value, 10);
       calculator[operator](screenValue);
       $screen.val(calc);
       operator = '';
+      // press operator
     } else if ((e.shiftKey && (e.keyCode === 187 || e.keyCode === 56)) ||
     (!e.shiftKey && (e.keyCode === 191 || e.keyCode === 189))) {
       $(this).submit();
@@ -124,6 +111,25 @@ $(() => {
       screenValue += $buttonValue;
       $screen.val(screenValue);
       $screen.focus();
+    }
+  });
+
+  $screen.focus();
+
+  // Add input validation to only allow numbers and operators
+  $screen.keydown((e) => {
+    // Allow: backspace, delete, escape and enter
+    if ($.inArray(e.keyCode, [8, 9, 27, 13, 110]) !== -1 ||
+    // Allow .
+    (!e.shiftKey && e.keyCode === 190) ||
+    // Allow: Ctrl+A, Command+A
+    (e.keyCode === 65 && (e.ctrlKey === true || e.metaKey === true)) ||
+    // Allow: home, end, left, right, down, up
+    (e.keyCode >= 35 && e.keyCode <= 40)) {
+      return;
+    }
+    if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+      e.preventDefault();
     }
   });
 });
